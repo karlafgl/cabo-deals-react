e02ecd83d2bc58166aaa4c0f5d15301b1bc13b00
Michael Guild <michaelguild13@gmail.com>
2019-06-04 22:51:52 -0400

    changing App to be functional

--- src/App.js
+++ src/App.js
@@ -4,23 +4,14 @@ import Navigation from './components/Navigation'
 import Home from './components/Home'
 import SeaAdventure from './components/SeaAdventure';
 
-
-
-class App extends Component {
-
-  render(){
-
-    return (
-      <BrowserRouter>
-        <div>
-          <Navigation />
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/sea-adventures" render={()=> <SeaAdventure />}/>
-        </div>
-      </BrowserRouter>
-);
-  }
-  
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Navigation />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/sea-adventures" render={()=> <SeaAdventure />}/>
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
