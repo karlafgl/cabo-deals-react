0850788e51c0088be2620d4cc33b1b26a7259a24
Michael Guild <michaelguild13@gmail.com>
2019-06-04 22:50:21 -0400

    fixed className error

--- src/components/SearchActivity.js
+++ src/components/SearchActivity.js
@@ -3,8 +3,8 @@ import React from 'react'
 const SearchActivity = ({onInputChange}) => {
     return(
         <div className="pa2 tc">
-            <p><i class="fas fa-2x fa-search"></i></p>
-            <input type="search" placeholder="Search your Activity" 
+            <p><i className="fas fa-2x fa-search"></i></p>
+            <input type="search" placeholder="Search your Activity"
             className="pa3 ba b--green bg-lightest-blue"
             onChange={onInputChange}
             />
